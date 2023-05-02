7837ce25f2a59dd2083a8a00d57da3b1fa5dc405
powerninja <asdyuy23@gmail.com>
2023-05-03 00:01:12 +0900

    ざっとhtml記載

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export const App = () => {
-  return <div>test</div>;
+  return (
+    <div
+      style={{
+        height: "auto",
+        margin: "0 auto",
+        maxWidth: 800,
+        width: "100%",
+      }}
+    >
+      <h1>パス変換</h1>
+      <div className="d-flex flex-row">
+        <div className="form-group my-box w-25">
+          <label>windows Path:</label>
+          <textarea id="textarea" className="form-control"></textarea>
+        </div>
+
+        <div className="form-group my-box w-25">
+          <label>mac Path:</label>
+          <textarea id="textarea" className="form-control"></textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
