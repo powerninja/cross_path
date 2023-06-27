1b3df369912d0d945126031c8c4898e09d79dd50
powerninja <asdyuy23@gmail.com>
2023-06-27 22:48:11 +0900

    add:app barを追加

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import InputAdornment from '@mui/material/InputAdornment';
 import CheckIcon from '@mui/icons-material/Check';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
 
 export const App = () => {
   //TextFieldの初期値
@@ -87,78 +90,100 @@ export const App = () => {
   };
 
   return (
-    <div
-      style={{
-        height: 'auto',
-        margin: '0 auto',
-        maxWidth: 1000,
-        width: '100%',
-      }}
-    >
-      <Typography variant="h4" gutterBottom>
-        ファイルパス変換
-      </Typography>
-      {/* <h1>ファイルパス変換</h1> */}
-      <div className="d-flex flex-row justify-content-around">
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': { m: 1, width: '40ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-multiline-static"
-            label="Windows Path"
-            multiline
-            rows={8}
-            value={checkConvertedWinPath ? winPath : convertedWinPath}
-            onChange={(event) => {
-              setPathInput(event.target.value, true);
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="sticky" color="transparent">
+          <Toolbar>
+            <LaptopMacIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              ファイルパス変換
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div
+        style={{
+          height: 'auto',
+          margin: '0 auto',
+          maxWidth: 1000,
+          width: '100%',
+        }}
+      >
+        <div className="d-flex flex-row justify-content-around">
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '40ch' },
             }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Tooltip title="Copy to Clipboard" placement="top" arrow>
-                    <IconButton color="primary" size="small" onClick={() => copyToClipboard(resultWinText, true)}>
-                      {checkCopyWinFlag ? <CheckIcon></CheckIcon> : <ContentCopyIcon fontSize="small" />}
-                    </IconButton>
-                  </Tooltip>
-                </InputAdornment>
-              ),
-            }}
-          />
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="outlined-multiline-static"
+              label="Windows Path"
+              multiline
+              rows={8}
+              value={checkConvertedWinPath ? winPath : convertedWinPath}
+              onChange={(event) => {
+                setPathInput(event.target.value, true);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Tooltip title="Copy to Clipboard" placement="top" arrow>
+                      <IconButton color="primary" size="small" onClick={() => copyToClipboard(resultWinText, true)}>
+                        {checkCopyWinFlag ? <CheckIcon></CheckIcon> : <ContentCopyIcon fontSize="small" />}
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-          <TextField
-            id="outlined-multiline-static"
-            label="Mac Path"
-            multiline
-            rows={8}
-            value={checkConvertedMacPath ? macPath : convertedMacPath}
-            onChange={(event) => {
-              setPathInput(event.target.value, false);
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Tooltip title="Copy to Clipboard" placement="top" arrow>
-                    <IconButton color="primary" size="small" onClick={() => copyToClipboard(resultMacText, false)}>
-                      {checkCopyMacFlag ? <CheckIcon></CheckIcon> : <ContentCopyIcon fontSize="small" />}
-                    </IconButton>
-                  </Tooltip>
-                </InputAdornment>
-              ),
-            }}
-          />
+            <TextField
+              id="outlined-multiline-static"
+              label="Mac Path"
+              multiline
+              rows={8}
+              value={checkConvertedMacPath ? macPath : convertedMacPath}
+              onChange={(event) => {
+                setPathInput(event.target.value, false);
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Tooltip title="Copy to Clipboard" placement="top" arrow>
+                      <IconButton color="primary" size="small" onClick={() => copyToClipboard(resultMacText, false)}>
+                        {checkCopyMacFlag ? <CheckIcon></CheckIcon> : <ContentCopyIcon fontSize="small" />}
+                      </IconButton>
+                    </Tooltip>
+                  </InputAdornment>
+                ),
+              }}
+            />
 
-          <div className="button-container">
-            <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => clearPath()}>
-              クリア
-            </Button>
-          </div>
-        </Box>
+            <div className="button-container">
+              <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => clearPath()}>
+                クリア
+              </Button>
+            </div>
+          </Box>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
